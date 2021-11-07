250cd6c29b6ba613c4c5215b719c04a9d8ae6583
Lin Meyer <lin.meyer@redbox.com>
2021-11-06 22:58:41 -0500

    Added battery level

--- owlet_monitor/exporters/prometheus.py
+++ owlet_monitor/exporters/prometheus.py
@@ -12,15 +12,15 @@ class PrometheusExporter(BaseExporter):
         self.metric_heart = Gauge('owlet_heart_rate', 'Heart rate', ['dsn'])
         self.metric_oxy = Gauge('owlet_oxygen_level', 'Oxygen Status', ['dsn'])
         self.metric_wiggle_level = Gauge('owlet_wiggle_level', 'Wiggle level (movement)', ['dsn'])
-        self.metric_charge_status = Gauge('owlet_charge_status', 'Charging status', ['dsn'])
         self.metric_owlet_api_updates = Counter('owlet_api_updates', 'Number of Owlet API updates', ['dsn'])
+        self.metric_battery_level = Counter('owlet_battery_level', 'Battery level (percentage)', ['dsn'])
 
     def export(self, status: OwletStatus):
         """Maps OwletStatus to Prometheus metrics and updates them"""
         self.metric_base_station_on.labels(status.device_sn).set(status.base_station_on)
         self.metric_heart.labels(status.device_sn).set(status.heart_rate)
         self.metric_oxy.labels(status.device_sn).set(status.oxygen_level)
-        self.metric_charge_status.labels(status.device_sn).set(status.charge_status)
+        self.metric_battery_level.labels(status.device_sn).set(status.battery_level)
         self.metric_owlet_api_updates.labels(status.device_sn).inc()
         self.metric_wiggle_level.labels(status.device_sn).set(status.movement_level)
 
